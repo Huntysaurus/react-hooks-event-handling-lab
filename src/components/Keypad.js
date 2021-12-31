e2167a06be848dd157350cb7cc236adba3d2047b
Jacob <yawjacob@yahoo.com>
2021-12-30 18:59:13 -0600

    completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+    return (
+        <form>
+            <input type="password" placeholder="Type Something" onChange={() => console.log('Entering password...')} />
+        </form>
+    )
+}
+
+export default Keypad
